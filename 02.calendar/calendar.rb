0002ab23d8bc97373bc360a3ccd779d8fa0df8ae
Toshiyasu Nonohara <hh0648075@gmail.com>
2021-02-05 06:46:38 +0900

    modified based on rubocop

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,35 +1,44 @@
 #!/usr/bin/ruby
+# frozen_string_literal: true
+
 require 'date'
 require 'optparse'
 
 options = ARGV.getopts('m:y:')
-year = options["y"] || Date.today.year
-month = options["m"] || Date.today.month
+year = options['y'] || Date.today.year
+month = options['m'] || Date.today.month
 
 puts "　　　　#{month}月　#{year}"
-puts "日　月　火　水　木　金　土"
+puts '日　月　火　水　木　金　土'
 first_day = Date.parse("#{year}-#{month}-1")
 final_day = Date.new(year.to_i, month.to_i, -1)
 second_day = Date.parse("#{year}-#{month}-2")
 
 days_array = []
 if first_day.monday?
-  days_array.push("     #{first_day.day}")
+  spaces = ' ' * 5
+  days_array.push("#{spaces}#{first_day.day}")
 elsif first_day.tuesday?
-  days_array.push("         #{first_day.day}")
+  spaces = ' ' * 9
+  days_array.push("#{spaces}#{first_day.day}")
 elsif first_day.wednesday?
-  days_array.push("             #{first_day.day}")
+  spaces = ' ' * 13
+  days_array.push("#{spaces}#{first_day.day}")
 elsif first_day.thursday?
-  days_array.push("                 #{first_day.day}")
+  spaces = ' ' * 17
+  days_array.push("#{spaces}#{first_day.day}")
 elsif first_day.friday?
-  days_array.push("                     #{first_day.day}")
+  spaces = ' ' * 21
+  days_array.push("#{spaces}#{first_day.day}")
 elsif first_day.saturday?
-  days_array.push("                         #{first_day.day}\n")
+  spaces = ' ' * 25
+  days_array.push("#{spaces}#{first_day.day}\n")
 else first_day.sunday?
-  days_array.push("#{first_day.day}　")
+  spaces = ' ' * 2
+  days_array.push("#{first_day.day}#{spaces}")
 end
 
-days = (second_day..final_day)  
+days = (second_day..final_day)
 days.each do |day|
   if day.saturday?
     days_array.push("#{day.day}\n")
@@ -39,7 +48,7 @@ days.each do |day|
 end
 
 days_array.each do |day|
-  if day.to_i > 0 && day.to_i < 10
+  if day.to_i.positive? && day.to_i < 10
     print(" #{day} ")
   else
     print("#{day} ")
